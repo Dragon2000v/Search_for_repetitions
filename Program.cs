e6ddb0be235bfaeb4c9ce0fcbb577d17da2ced5e
Vitaly <shmatko064@gmail.com>
2023-10-30 17:05:52 +0200

    Finish

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+var word = "Hello world!";
+var characters = word.ToCharArray();
+
+var charMap = new Dictionary<char, int>();
+foreach (char ch in characters)
+{
+    if (charMap.ContainsKey(ch))
+    {
+        charMap[ch]++;
+    }
+    else
+    {
+        charMap[ch] = 1;
+    }
+}
+
+Console.WriteLine($"Список повторяющихся символов в строке'{word}':");
+foreach (var entry in charMap)
+{
+    if (entry.Value > 1)
+    {
+        Console.WriteLine($"{entry.Key} : {entry.Value}");
+    }
+}
